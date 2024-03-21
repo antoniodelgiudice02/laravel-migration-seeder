7deb12c448cc121f0149fcbf47f2a2433b7c968b
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-03-21 16:03:49 +0100

    add columns

--- database/migrations/2024_03_21_145650_create_trains_table.php
+++ database/migrations/2024_03_21_145650_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->dateTime('Orario_di_partenza');
+            $table->dateTime('Orario_di_arrivo');
+            $table->string('Codice_treno', 10);
+            $table->tinyInteger('Numero_di_carrozze');
+            $table->boolean('In_orario')->default(true);
+            $table->boolean('Cancellato')->default(false);
             $table->timestamps();
         });
     }
